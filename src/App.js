ec891360c9be69b9c7ddd900cbfc8e7cbfd4e2d8
awilkosz <aurelien.wilkosz@laposte.net>
2021-04-16 17:02:31 +0200

    préparation du front

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import {useState} from 'react';
 
-function App() {
+const App = () => {
+  const [msg, setMsg] = useState('');
+
+  const handleClick = async () => {
+    const data = await fetch('/api/test');
+    const json = await data.json();
+    const msg = json.msg;
+
+    setMsg(msg);
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="App"> 
+      <button onClick={handleClick}>
+        Hello World !
+      </button>
+      <p>{msg}</p>
     </div>
   );
 }
